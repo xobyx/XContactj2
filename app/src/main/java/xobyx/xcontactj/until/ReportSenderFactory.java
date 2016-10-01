6ef2ba4fa15df32369059e9a33ac57addde9d855
xobyx <xobyxm@hotmail.com>
2016-10-01 12:08:10 +0300

    add m acra report factory

--- app/src/main/java/xobyx/xcontactj/until/ReportSenderFactory.java
+++ app/src/main/java/xobyx/xcontactj/until/ReportSenderFactory.java
@@ -11,7 +11,7 @@ import org.acra.sender.ReportSenderFactory;
  * Created by xobyx on 2/16/2016.
  * For xobyx.xcontactj.until/XContactj
  */
-public class ReportSenderFac implements ReportSenderFactory {
+public class ReportSenderFactory implements org.acra.sender.ReportSenderFactory {
     @NonNull
     @Override
     public ReportSender create(Context context, ACRAConfiguration config) {
